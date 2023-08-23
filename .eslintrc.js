394c452f995af36feab2436632d524189c4a6148
yehtony <yehtony880208@outlook.com>
2023-08-23 16:43:48 +0800

    add contexts/context.js

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['google', 'plugin:react/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'quotes': ['error', 'single'],
+    'linebreak-style': ['error', 'windows'],
+    'require-jsdoc': 'off',
+    'object-curly-spacing': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-key': 'off',
+    'max-len': 'off',
+    'react/prop-types': 'off',
+  },
+};
