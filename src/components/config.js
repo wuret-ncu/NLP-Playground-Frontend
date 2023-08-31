3db32ae4d777d5c4cccdfe33182b0de29a13c4d3
yehtony <yehtony880208@outlook.com>
2023-09-01 03:25:07 +0800

    App.css('tooltip') + system.js('tooltip'+ layout revise)

--- src/components/config.js
+++ src/components/config.js
@@ -1,16 +1,24 @@
 import { TbAdjustmentsHeart } from 'react-icons/tb';
 import { Context } from '../contexts/context.js';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
+import { TbInfoCircle } from 'react-icons/tb';
 
 function Config() {
   const { parameters, setParameters } = useContext(Context);
+  const [tempInput, setTempInput] = useState('');
+  const [stopSequences, setStopSequences] = useState({});
 
   // 處理參數值的變更
-  const handleParameterChange = (param, key, newValue) => {
+  const handleParameterChange = async (param, key, newValue) => {
+    console.log(newValue);
     // 確認新值是否是有效的數字，以及是否在設定的範圍內
-    if (!isNaN(newValue) && newValue >= parameters[param].min && newValue <= parameters[param].max) {
+    if (
+      !isNaN(newValue) &&
+      newValue >= parameters[param].min &&
+      newValue <= parameters[param].max
+    ) {
       // 使用 setParameters 更新參數狀態
-      setParameters((prevParameters) => ({
+      await setParameters((prevParameters) => ({
         ...prevParameters,
         [param]: {
           ...prevParameters[param],
@@ -18,62 +26,137 @@ function Config() {
         },
       }));
     }
+    console.log(parameters);
+  };
+  const handleTempInput = (param, value) => {
+    setTempInput(value);
+  };
+
+  const handleAddStopSequence = (param) => {
+    if (tempInput.trim() !== '') {
+      setStopSequences((prevSequences) => ({
+        ...prevSequences,
+        [param]: [...(prevSequences[param] || []), tempInput],
+      }));
+      setTempInput('');
+    }
+  };
+
+  const handleRemoveStopSequence = (param, index) => {
+    setStopSequences((prevSequences) => ({
+      ...prevSequences,
+      [param]: prevSequences[param].filter((_, i) => i !== index),
+    }));
   };
 
   return (
     <div>
-      <div className='grid grid-rows-2-23 h-85vh gap-5'>
-        <div className='grid grid-flow-col items-center'>
-          <div className='grid grid-flow-col justify-start items-center'>
+      <div className="grid grid-rows-2-23 h-85vh gap-5">
+        <div className="grid grid-flow-col items-center">
+          <div className="grid grid-flow-col justify-start items-center">
             {/* 圖標 */}
-            <TbAdjustmentsHeart className='h-6 w-6 mx-1' />
+            <TbAdjustmentsHeart className="h-6 w-6 mx-1" />
             {/* 標題 */}
-            <p className='text-xl'>參數配置</p>
+            <p className="text-xl">參數配置</p>
           </div>
         </div>
-        <div className='grid grid-flow-row gap-5 auto-rows-min mx-1'>
+        <div className="grid grid-flow-row gap-5 auto-rows-min mx-1">
           {/* 遍歷參數，創建滑動條和輸入框 */}
-          {Object.entries(parameters).map(([param, { value, min, max }]) => (
-            param !== '停止序列' && (
-              <div key={param}>
-                <div className='grid grid-flow-col items-center justify-between'>
-                  {/* 參數名稱 */}
-                  {param}
-                  {/* 輸入框 */}
+          {Object.entries(parameters).map(
+              ([param, { value, min, max, step }]) =>
+              param !== '停止序列' ? (
+                <div key={param}>
+                  <div className="grid grid-flow-col items-center justify-between">
+                    <div>
+                      <div className="grid grid-flow-col justify-start items-center relative">
+                        {/* 參數名稱 */}
+                        {param}
+                        <div className="custom-tooltip-container over ">
+                          <TbInfoCircle className="stroke-slate-400 hover:stroke-sky-600"></TbInfoCircle>
+                          <span className="custom-tooltip overflow mb-1">
+                            提供模型指示，說明其應如何運作，以及產生回應時應參考的任何內容。您可以描述小幫手的特質、告訴小幫手應該和不應該回答什麼，並告訴它如何格式化回應。此區段沒有權杖限制，但會包含在每一個
+                            API 呼叫中，因此它計入總權杖限制。
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                    {/* 輸入框 */}
+                    <input
+                      type="text"
+                      placeholder="輸入數值"
+                      value={isNaN(value) ? '' : value}
+                      onChange={(e) =>
+                        handleParameterChange(param, 'value', e.target.value)
+                      }
+                      className="input input-bordered w-14 h-7 text-sm px-1"
+                    />
+                  </div>
+                  {/* 滑動條 */}
                   <input
-                    type='text'
-                    placeholder='輸入數值'
-                    value={isNaN(value) ? '' : value}
-                    onChange={(e) => handleParameterChange(param, 'value', e.target.value)}
-                    className='input input-bordered w-14 h-7 text-sm'
+                    type="range"
+                    min={min}
+                    max={max}
+                    step={step}
+                    value={value}
+                    onChange={(e) =>
+                      handleParameterChange(
+                          param,
+                          'value',
+                          parseFloat(e.target.value),
+                      )
+                    }
+                    className="range range-xs"
                   />
                 </div>
-                {/* 滑動條 */}
-                <input
-                  type='range'
-                  min={min}
-                  max={max}
-                  step={0.1}
-                  value={value}
-                  onChange={(e) => handleParameterChange(param, 'value', parseFloat(e.target.value))}
-                  className='range range-xs'
-                />
-              </div>
-            )
-          ))}
-          {/* 停止序列輸入框 */}
-          <div>
-            停止序列
-            <input
-              type='text'
-              placeholder='停止序列'
-              className='input input-bordered w-full h-10 mt-1'
-            />
-          </div>
+              ) : (
+                <div key={param}>
+                  <div className="grid grid-flow-col justify-between items-center mb-1">
+                    {/* 參數名稱 */}
+                    {param}
+                  </div>
+                  <div className= "w-64 h-auto border-2 rounded-lg flex flex-wrap items-center p-1">
+                    {stopSequences[param]?.map((sequence, index) => (
+                      <div key={index} className="m-1 grid grid-flow-col items-center">
+                        <button
+                          onClick={() =>
+                            handleRemoveStopSequence(param, index)
+                          }
+                          className="btn btn-xs"
+                        >
+                          <div>{sequence}</div>
+                          <span className="text-lg">×</span>
+                        </button>
+                      </div>
+                    ))}
+                    <input
+                      type="text"
+                      placeholder="輸入停止序列"
+                      onChange={(e) => handleTempInput(param, e.target.value)}
+                      value={tempInput}
+                      className="input px-2 border-white"
+                      style={{
+                        fontSize: '14px',
+                        outline: 'none',
+                      }}
+                    />
+                    <button
+                      onClick={() => handleAddStopSequence(param)}
+                      className="btn btn-outline btn-sm ml-auto mr-2"
+                    >
+                      新增
+                    </button>
+                  </div>
+                </div>
+              ),
+          )}
           {/* 目前 tokens 計數 */}
-          <div className='grid mt-10'>
+          <div className="grid mt-10">
             目前 tokens 計數
-            <progress className='progress h-3 mb-1' value='40' max='100'></progress>
+            <progress
+              className="progress h-3 mb-1"
+              value="40"
+              max="100"
+            ></progress>
             <p>125 / 4000</p>
           </div>
         </div>
--- src/contexts/context.js
+++ src/contexts/context.js
@@ -5,12 +5,13 @@ export const ContextProvider = (props) => {
   const [message, setMessage] = useState({});
   const [chatlog, setChatlog] = useState([]);
   const [parameters, setParameters] = useState({
-    回應上限: { value: 0, min: 0, max: 16000 },
-    溫度: { value: 0.0, min: 0, max: 1.0 },
-    頂端P: { value: 0.0, min: 0, max: 1.0 },
-    頻率罰則: { value: 0.0, min: 0, max: 2.0 },
-    目前狀態罰則: { value: 0.0, min: 0, max: 2.0 },
-    停止序列: { value: '', min: 0, max: 100 },
+    回應上限: { value: 800, min: 0, max: 16000, step: 1 },
+    溫度: { value: 0.7, min: 0, max: 1.0, step: 0.01},
+    頂端P: { value: 0.95, min: 0, max: 1.0, step: 0.01 },
+    包含過去的訊息: { value: 10, min: 1, max: 20, step: 1},
+    停止序列: { value: [], min: 0, max: 4 },
+    頻率罰則: { value: 0.0, min: 0, max: 2.0, step: 0.01 },
+    目前狀態罰則: { value: 0.0, min: 0, max: 2.0, step: 0.01 },
   });
 
 
